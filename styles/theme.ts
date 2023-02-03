9c3450e0aa175a1099f5060a264f76e3d0e8472f
Ivan Alburquerque <ialburquerque@fandom.com>
2023-02-03 00:53:32 -0400

    I10 :: Fix TS version not supporting string interpolation enums

--- styles/theme.ts
+++ styles/theme.ts
@@ -22,26 +22,25 @@ interface ThemeInterface {
 }
 
 
-enum Size {
-	MOBILE_SMALL = '320px',
-	MOBILE = '375px',
-	MOBILE_LARGE = '425px',
-	TABLET = '768px',
-	DESKTOP = '1024px',
-	DESKTOP_LARGE = '1440px',
-	DESKTOP_XLARGE = '2560px',
+const Size = {
+	MOBILE_SMALL: '320px',
+	MOBILE: '375px',
+	MOBILE_LARGE: '425px',
+	TABLET: '768px',
+	DESKTOP: '1024px',
+	DESKTOP_LARGE: '1440px',
+	DESKTOP_XLARGE: '2560px',
 };
 
-enum Device {
-	MOBILE_SMALL = `(min-width: ${Size.MOBILE_SMALL})`,
-	MOBILE = `(min-width: ${Size.MOBILE})`,
-	MOBILE_LARGE = `(min-width: ${Size.MOBILE_LARGE})`,
-	TABLET = `(min-width: ${Size.TABLET})`,
-	DESKTOP = `(min-width: ${Size.DESKTOP})`,
-	DESKTOP_LARGE = `(min-width: ${Size.DESKTOP_LARGE})`,
-	DESKTOP_XLARGE = `(min-width: ${Size.DESKTOP_XLARGE})`,
+const Device = {
+	MOBILE_SMALL: `(min-width: ${Size.MOBILE_SMALL})`,
+	MOBILE: `(min-width: ${Size.MOBILE})`,
+	MOBILE_LARGE: `(min-width: ${Size.MOBILE_LARGE})`,
+	TABLET: `(min-width: ${Size.TABLET})`,
+	DESKTOP: `(min-width: ${Size.DESKTOP})`,
+	DESKTOP_LARGE: `(min-width: ${Size.DESKTOP_LARGE})`,
+	DESKTOP_XLARGE: `(min-width: ${Size.DESKTOP_XLARGE})`,
 };
-
 
 // default theme options for both light and dark theme
 const defaultTheme = {
