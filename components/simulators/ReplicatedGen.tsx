91ea4e51c42a6a4c18f47369eaa590bb53845bdb
Carlos Medina <cmedina.developer@gmail.com>
2023-02-06 15:08:13 -0400

    Add missing prop for LegendContainer

--- components/simulators/ReplicatedGen.tsx
+++ components/simulators/ReplicatedGen.tsx
@@ -229,6 +229,7 @@ function HomePage() {
 					alleleResults={context.alleleResults}
 					genoTypeResults={null}
 					settings={context.settingResults}
+					graphNumber={1}
 				/>
 
 				<HighChart lines={context.genoTypeResults} title={'Graph 2: Genotype Frequency Change Over Generations'} />
@@ -236,6 +237,7 @@ function HomePage() {
 					alleleResults={context.alleleResults}
 					genoTypeResults={context.genoTypeResults}
 					settings={context.settingResults}
+					graphNumber={2}
 				/>
 
 				<ButtonWrapper marginTop={45}>
