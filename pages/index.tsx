68f4f6f7751e86f71b5841b4d7fe8b2a9cf6b484
Ivan Alburquerque <albur.ivan@outlook.com>
2023-09-03 21:56:34 -0400

    Feature :: Redesign landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,152 +1,136 @@
-import Link from 'next/link';
-
+import NextLink from 'next/link';
+import { Box, Button, ButtonGroup, Img, Link, Text, Tooltip, useColorModeValue } from '@chakra-ui/react';
 import MainWrapper from '../components/MainWrapper';
-
-import { Box, Button, ButtonGroup, Text, Tooltip, useColorModeValue, ListItem, UnorderedList } from '@chakra-ui/react';
-
-import FirstSectionSlide from '../components/landing-page/FirstSectionSlide';
-import SecondSectionSlide from '../components/landing-page/SecondSectionSlide';
-import ThirdSectionSlide from '../components/landing-page/ThirdSectionSlide';
-import FourthSectionSlide from '../components/landing-page/FourthSectionSlide';
-import FifthSectionSlide from '../components/landing-page/FifthSectionSlide';
 
 function Index() {
 	return (
 		<MainWrapper>
-			<Box as="section" p={7} maxWidth={{ base: '550px', md: '786px', lg: '860px', xl: '1080px' }} mx={{ sm: 'auto' }}>
-				<Text
-					color="text"
-					fontWeight={'extrabold'}
-					fontSize={{ base: '24px', md: '40px', lg: '56px' }}
-					align="center"
-					padding={{ base: 2, md: 0 }}
-					marginY={{ base: 4, md: 6, lg: 10 }}
-				>
-					Population Genetics Explorer
-				</Text>
-				<Text color="text" marginY={{ base: 3, md: 4 }}>
-					Population genetics is the study of the{' '}
-					{
-						<Tooltip
-							label="Variants of a particular gene or DNA region"
-							aria-label="A tooltip"
-							color={useColorModeValue('black', 'white')}
-							backgroundColor={useColorModeValue('purple.200', 'purple.500')}
-							padding={2}
-							hasArrow
-						>
-							<Text
-								as="span"
-								display="inline-block"
-								color="text"
-								textDecoration="wavy underline"
-								textDecorationColor="purple.300"
-								textUnderlineOffset={2}
-								fontWeight={800}
-							>
-								alleles
-							</Text>
-						</Tooltip>
-					}{' '}
-					within a biological population. All living things, including us, belong to a population: a group of
-					individuals of the same species that live in the same area and potentially reproduce together. Biologists
-					explore the frequency of alleles in different places that populations live.
-				</Text>
-				<Text color="text" marginY={{ base: 3, md: 4 }}>
-					They also use population genetics to examine how natural selection, the movement of individuals between
-					populations, or other factors change the frequency of alleles in a population over time. A change in the
-					frequency of alleles in a population over time is evolution, so population genetics is crucial for
-					understanding how evolution works.
-				</Text>
-				<Text
-					color="text"
-					align="center"
-					fontWeight={'extrabold'}
-					fontSize={{ base: '24px', md: '30px', lg: '40px' }}
-					marginTop={{ base: 4, md: 16, lg: 24 }}
-					padding={{ base: 2, md: 0 }}
-				>
-					You can use Population Genetics Explorer to test questions like the following:
-				</Text>
+			<Box
+				as="section"
+				padding={7}
+				marginTop={{
+					md: '7%',
+					lg: '0',
+				}}
+				maxWidth={{ md: '90%', lg: '80%' }}
+				mx={{ sm: 'auto' }}
+			>
 				<Box
-					as="section"
-					position="relative"
-					left="50%"
-					right="50%"
-					mx="-50vw"
-					w="100vw"
-					py={{ base: '20px', md: '40px' }}
-					// px={{ base: '0', md: '30px' }}
-					// bg={useColorModeValue('gray.100', ' gray.700')}
-				>
-					{/* Mice */}
-					<FirstSectionSlide />
-
-					{/* Sickled Cell */}
-					<SecondSectionSlide />
-
-					{/* Lynx */}
-					<ThirdSectionSlide />
-
-					{/* Mosquito */}
-					<FourthSectionSlide />
-
-					{/* Bluebird */}
-					<FifthSectionSlide />
-				</Box>
-
-				<Text color="text" marginY={{ base: 3, md: 4 }}>
-					Tracking whole genomes in lots of individuals over time is complicated. To explore questions like these,
-					biologists use mathematical models. Like all models, the one used in this simulation makes some simplifying
-					assumptions, such as the following:
-				</Text>
-				<UnorderedList
-					spacing={4}
-					marginY={{ base: 10, md: 10, lg: 10 }}
-					ml={{
-						base: 4,
+					display="grid"
+					gridTemplateColumns={{
+						base: '1fr',
+						md: '1fr 1fr',
+					}}
+					flexDirection={{
+						base: 'column',
+						md: 'row',
+					}}
+					justifyContent={{
+						base: 'center',
+						md: 'space-between',
+					}}
+					placeItems="center"
+					gap={{
+						base: 5,
 						md: 10,
-						lg: 16,
 					}}
 				>
-					<ListItem>
-						The simulation tracks one specific gene region (or locus) with only two alleles. The symbol &quot;A
-						<sub>1</sub>&quot; represents the allele being analyzed. The symbol &quot;A<sub>2</sub>&quot; is the other
-						allele.
-					</ListItem>
-					<ListItem>
-						The individuals in this population have two copies of a gene region. The copies can be the same or different
-						allele.
-					</ListItem>
-					<ListItem>
-						Individuals in the population reproduce sexually. These assumptions make the model easier to understand.
-						Although it is simpler than reality, it provides a powerful framework that biologists use to explore and
-						make predictions about evolution.
-					</ListItem>
-				</UnorderedList>
-				<Text color="text" marginY={{ base: 3, md: 10, lg: 16 }}>
-					In this Click & Learn, you&apos;ll explore a population genetics simulation. Use the simulation to explore and
-					test your own questions on the impacts of natural selection,population size, migration and more on allele or
-					genotype frequencies.
-				</Text>
-				<ButtonGroup
-					w="100%"
-					display={'flex'}
-					flexDirection={{ base: 'column', md: 'row' }}
-					justifyContent={{ base: 'center', md: 'space-around' }}
-					alignItems={'center'}
-					marginTop={25}
-					marginBottom={25}
-					spacing={{ base: 0, md: 4 }}
-				>
-					<Button as={Link} variant="primary" href="/individual">
-						Go to Individual Simulations
-					</Button>
+					<Box display="flex" flexDirection="column" alignItems={{ base: 'center', md: 'start' }} gap="15px">
+						<Text
+							color="text"
+							align={{ base: 'center', md: 'start' }}
+							fontWeight={'extrabold'}
+							fontSize={{ base: '24px', md: '36px', lg: '50px' }}
+							marginTop={{ base: 4, md: 6, lg: 12 }}
+							lineHeight={{ base: '1.2', md: '1.1' }}
+						>
+							Dive into Population Genetics Science
+						</Text>
+						<Text color="text" marginY={{ base: 3 }} width="80%" align={{ base: 'center', md: 'start' }}>
+							Welcome to the Population Genetics Explorer, your gateway to understanding genetic evolution. Dive into
+							the fascinating world of genetics and explore the dynamic frequencies of alleles, natural selection, and
+							more through mathematical simulations within populations.
+						</Text>
+						<Button
+							as={NextLink}
+							href="/introduction"
+							variant="primary"
+							bgGradient="linear(to-r, purple.400, purple.500)"
+							_hover={{
+								bgGradient: 'linear(to-r, purple.500, purple.600)',
+								shadow: 'xl',
+							}}
+							alignSelf={{
+								base: 'center',
+								md: 'start',
+							}}
+							animation="all ease-in 5.2s"
+							borderRadius="25px"
+							shadow="md"
+						>
+							Learn More
+						</Button>
+					</Box>
 
-					<Button as={Link} variant="primary" href="/replicated" marginTop={{ base: 4, md: 0 }}>
-						Go to Replicated Simulations
-					</Button>
-				</ButtonGroup>
+					<Img
+						src="/images/homepage-hero-image.svg"
+						height={{
+							base: '250px',
+							md: '350px',
+							lg: '400px',
+							xl: '450px',
+						}}
+						placeSelf="center"
+					/>
+				</Box>
+
+				<Box
+					maxWidth={{
+						base: '100%',
+						md: '90%',
+						lg: '80%',
+					}}
+					marginX="auto"
+				>
+					<Text color="text" marginY={{ base: 6, md: 8, lg: 16 }} textAlign="center">
+						Take a hands-on approach to understanding genetic evolution. Use the simulators to explore and test your own
+						questions on the impacts of natural selection, population size, and more.
+					</Text>
+					<ButtonGroup
+						width={{
+							base: '100%',
+							xl: '80%',
+						}}
+						display={'flex'}
+						flexDirection={{ base: 'column', md: 'row' }}
+						justifyContent={{ base: 'center', md: 'space-around' }}
+						alignItems={'center'}
+						marginBottom={25}
+						marginX="auto"
+						spacing={{ base: 0, md: 4 }}
+					>
+						<Button as={NextLink} variant="primary" href="/individual">
+							Go to Individual Simulations
+						</Button>
+
+						<Button as={NextLink} variant="primary" href="/replicated" marginTop={{ base: 4, md: 0 }}>
+							Go to Replicated Simulations
+						</Button>
+					</ButtonGroup>
+					<Text color="text" textAlign="center">
+						Need any help? Check out our{' '}
+						<Link
+							as={NextLink}
+							href="/faq"
+							textDecoration="underline"
+							color={useColorModeValue('purple.500', 'purple.300')}
+							fontWeight="semibold"
+						>
+							FAQ page
+						</Link>{' '}
+						for answers to get you started.
+					</Text>
+				</Box>
 			</Box>
 		</MainWrapper>
 	);
---
+++ pages/introduction.tsx
@@ -0,0 +1,173 @@
+import Link from 'next/link';
+
+import MainWrapper from '../components/MainWrapper';
+
+import { Box, Button, ButtonGroup, Text, Tooltip, useColorModeValue, ListItem, UnorderedList } from '@chakra-ui/react';
+
+import FirstSectionSlide from '../components/landing-page/FirstSectionSlide';
+import SecondSectionSlide from '../components/landing-page/SecondSectionSlide';
+import ThirdSectionSlide from '../components/landing-page/ThirdSectionSlide';
+import FourthSectionSlide from '../components/landing-page/FourthSectionSlide';
+import FifthSectionSlide from '../components/landing-page/FifthSectionSlide';
+
+function Index() {
+	return (
+		<MainWrapper>
+			<Box as="section" p={7} maxWidth={{ base: '550px', md: '786px', lg: '860px', xl: '1080px' }} mx={{ sm: 'auto' }}>
+				<Text
+					color="text"
+					fontWeight={'extrabold'}
+					fontSize={{ base: '24px', md: '40px', lg: '56px' }}
+					align="center"
+					padding={{ base: 2, md: 0 }}
+					marginY={{ base: 4, md: 6, lg: 10 }}
+				>
+					Population Genetics Explorer
+				</Text>
+				<Text
+					color="text"
+					marginY={{ base: 3, md: 4 }}
+					align={{
+						base: 'center',
+						md: 'left',
+					}}
+				>
+					Population genetics is the study of the{' '}
+					{
+						<Tooltip
+							label="Variants of a particular gene or DNA region"
+							aria-label="A tooltip"
+							color={useColorModeValue('black', 'white')}
+							backgroundColor={useColorModeValue('purple.200', 'purple.500')}
+							padding={2}
+							hasArrow
+						>
+							<Text
+								as="span"
+								display="inline-block"
+								color="text"
+								textDecoration="wavy underline"
+								textDecorationColor="purple.300"
+								textUnderlineOffset={2}
+								fontWeight={800}
+							>
+								alleles
+							</Text>
+						</Tooltip>
+					}{' '}
+					within a biological population. All living things, including us, belong to a population: a group of
+					individuals of the same species that live in the same area and potentially reproduce together. Biologists
+					explore the frequency of alleles in different places that populations live.
+				</Text>
+				<Text
+					color="text"
+					marginY={{ base: 3, md: 4 }}
+					align={{
+						base: 'center',
+						md: 'left',
+					}}
+				>
+					They also use population genetics to examine how natural selection, the movement of individuals between
+					populations, or other factors change the frequency of alleles in a population over time. A change in the
+					frequency of alleles in a population over time is evolution, so population genetics is crucial for
+					understanding how evolution works.
+				</Text>
+				<Text
+					color="text"
+					align="center"
+					fontWeight={'extrabold'}
+					fontSize={{ base: '22px', md: '24px', lg: '30px' }}
+					marginTop={{ base: 4, md: 16 }}
+					padding={{ base: 2, md: 0 }}
+				>
+					You can use Population Genetics Explorer to test questions like the following:
+				</Text>
+				<Box
+					as="section"
+					display="flex"
+					flexDirection="column"
+					gap={{
+						base: 8,
+						md: 10,
+					}}
+					position="relative"
+					left="50%"
+					right="50%"
+					mx="-50vw"
+					w="100vw"
+					py={{ base: '20px' }}
+				>
+					{/* Mice */}
+					<FirstSectionSlide />
+
+					{/* Sickled Cell */}
+					<SecondSectionSlide />
+
+					{/* Lynx */}
+					<ThirdSectionSlide />
+
+					{/* Mosquito */}
+					<FourthSectionSlide />
+
+					{/* Bluebird */}
+					<FifthSectionSlide />
+				</Box>
+
+				<Text color="text" marginY={{ base: 3, md: 4 }}>
+					Tracking whole genomes in lots of individuals over time is complicated. To explore questions like these,
+					biologists use mathematical models. Like all models, the one used in this simulation makes some simplifying
+					assumptions, such as the following:
+				</Text>
+				<UnorderedList
+					spacing={4}
+					marginY={{ base: 10, md: 10, lg: 10 }}
+					ml={{
+						base: 4,
+						md: 10,
+						lg: 16,
+					}}
+				>
+					<ListItem>
+						The simulation tracks one specific gene region (or locus) with only two alleles. The symbol &quot;A
+						<sub>1</sub>&quot; represents the allele being analyzed. The symbol &quot;A<sub>2</sub>&quot; is the other
+						allele.
+					</ListItem>
+					<ListItem>
+						The individuals in this population have two copies of a gene region. The copies can be the same or different
+						allele.
+					</ListItem>
+					<ListItem>
+						Individuals in the population reproduce sexually. These assumptions make the model easier to understand.
+						Although it is simpler than reality, it provides a powerful framework that biologists use to explore and
+						make predictions about evolution.
+					</ListItem>
+				</UnorderedList>
+				<Text color="text" marginY={{ base: 3, md: 10, lg: 16 }} textAlign="center">
+					In this Click & Learn, you&apos;ll explore a population genetics simulation. Use the simulation to explore and
+					test your own questions on the impacts of natural selection,population size, migration and more on allele or
+					genotype frequencies.
+				</Text>
+				<ButtonGroup
+					w="100%"
+					display={'flex'}
+					flexDirection={{ base: 'column', md: 'row' }}
+					justifyContent={{ base: 'center', md: 'space-around' }}
+					alignItems={'center'}
+					marginTop={25}
+					marginBottom={25}
+					spacing={{ base: 0, md: 4 }}
+				>
+					<Button as={Link} variant="primary" href="/individual">
+						Go to Individual Simulations
+					</Button>
+
+					<Button as={Link} variant="primary" href="/replicated" marginTop={{ base: 4, md: 0 }}>
+						Go to Replicated Simulations
+					</Button>
+				</ButtonGroup>
+			</Box>
+		</MainWrapper>
+	);
+}
+
+export default Index;
