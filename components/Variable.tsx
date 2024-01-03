1e7a9806265412c98cfad94ed9cc4c8cf82b48b0
Josh <jrogan@wikia-inc.com>
2024-01-03 17:43:11 -0500

    Update more pages

--- components/Variable.tsx
+++ components/Variable.tsx
@@ -37,8 +37,48 @@ export function P0({...props}) {
 	return <Variable {...props}>p<sub>0</sub></Variable>
 }
 
+export function P({...props}) {
+	return <Variable {...props}>p</Variable>
+}
+
+export function P1({...props}) {
+	return <Variable {...props}>p<sub>1</sub></Variable>
+}
+
+export function PM({...props}) {
+	return <Variable {...props}>p<sub>M</sub></Variable>
+}
+
+export function PSquared({...props}) {
+	return <Variable {...props}>p<sup>2</sup></Variable>
+}
+
+export function QSquared({...props}) {
+	return <Variable {...props}>q<sup>2</sup></Variable>
+}
+
+export function Q({...props}) {
+	return <Variable {...props}>q</Variable>
+}
+
+export function Q0({...props}) {
+	return <Variable {...props}>q<sub>0</sub></Variable>
+}
+
 export function N({...props}) {
 	return <Variable {...props}>N</Variable>
+}
+
+export function F({...props}) {
+	return <Variable {...props}>F</Variable>
+}
+
+export function T({...props}) {
+	return <Variable {...props}>t</Variable>
+}
+
+export function M({...props}) {
+	return <Variable {...props}>m</Variable>
 }
 
 export function NSubE({...props}) {
@@ -53,6 +93,10 @@ export function NSubf({...props}) {
 	return <Variable {...props}>N<sub>f</sub></Variable>
 }
 
+
+export function NSubB({...props}) {
+	return <Variable {...props}>N<sub>B</sub></Variable>
+}
 // export function p({...props}) {
 // 	return <Variable {...props}>p</Variable>
 // }
@@ -79,4 +123,19 @@ export function WA1A2({...props}) {
 }
 export function WA2A2({...props}) {
 	return <Variable {...props}>W<sub>A2A2</sub></Variable>
+}
+export function V({...props}) {
+	return <Variable {...props}>v</Variable>
+}
+
+export function Mu({...props}) {
+	return <Variable {...props}>μ</Variable>
+}
+
+export function Alpha({...props}) {
+	return <Variable {...props}>α</Variable>
+}
+
+export function PowerTwo() {
+	return <sup>2</sup>
 }
